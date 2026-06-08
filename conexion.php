8955ae60aa6409b3a018875a74939738c85bbc39
micklanes <micklanessz@gmail.com>
2026-06-07 23:13:49 -0400

    Sincronización forzada de conexión y lógica de reportes

--- conexion.php
+++ conexion.php
@@ -129,7 +129,7 @@ class MiConexion extends PDO
 
 // --- Instancia de la conexión ---
 // LOCAL
-$db = new MiConexion("127.0.0.1", "bd_hospedajes", "root", "");
+//$db = new MiConexion("127.0.0.1", "bd_hospedajes", "root", "");
 
 // PRODUCCIÓN (HOST)
-//$db = new MiConexion("sdb-52.hosting.stackcp.net", "bdhospedajes-35303034893b", "bdhospedajes-35303034893b", "SCARYmovie1.");
+$db = new MiConexion("sdb-52.hosting.stackcp.net", "bdhospedajes-35303034893b", "bdhospedajes-35303034893b", "SCARYmovie1.");
