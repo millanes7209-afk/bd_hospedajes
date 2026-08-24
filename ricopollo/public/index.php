4aa32d50b8e6a4abaf172a55b513aecb2cdc62fa
micklanes <micklanessz@gmail.com>
2026-08-23 21:04:30 -0400

    Soporte de busqueda inteligente para vendor compartido en ricopollo/public/index.php

--- ricopollo/public/index.php
+++ ricopollo/public/index.php
@@ -6,15 +6,23 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// Register the Composer autoloader (busqueda inteligente de vendor)...
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    require __DIR__ . '/../../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../saas_scary/vendor/autoload.php')) {
+    require __DIR__ . '/../../saas_scary/vendor/autoload.php';
+} else {
+    require __DIR__ . '/../vendor/autoload.php';
+}
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
